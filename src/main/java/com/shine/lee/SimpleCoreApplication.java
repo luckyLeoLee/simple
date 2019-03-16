270b617632c53d35a4255759c1f9bbd2b9646d50
a <a@adeMacBook-Pro.local>
2019-03-16 22:17:24 +0800

    Initial commit

---
+++ src/main/java/com/shine/lee/SimpleCoreApplication.java
@@ -0,0 +1,13 @@
+package com.shine.lee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleCoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleCoreApplication.class, args);
+    }
+
+}
